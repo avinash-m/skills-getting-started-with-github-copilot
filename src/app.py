10ad214699567a4936a6cd7772873fb23e0d0823
avinash-m <39071770+avinash-m@users.noreply.github.com>
2026-02-11 18:05:23 +0000

    Add participant list display and styling to activity cards

--- src/app.py
+++ src/app.py
@@ -21,7 +21,6 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 
 # In-memory activity database
 activities = {
-    vities = {
         "Chess Club": {
             "description": "Learn strategies and compete in chess tournaments",
             "schedule": "Fridays, 3:30 PM - 5:00 PM",
@@ -75,8 +74,8 @@ activities = {
             "schedule": "Tuesdays, 3:30 PM - 5:00 PM",
             "max_participants": 20,
             "participants": ["lucas@mergington.edu"]
-        }
-    }
+        },
+    
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments",
         "schedule": "Fridays, 3:30 PM - 5:00 PM",
